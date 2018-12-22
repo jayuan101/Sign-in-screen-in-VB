4b776c6acc39f58e6e13916804df44191ef29b13
jayuan101 <43624484+jayuan101@users.noreply.github.com>
2018-12-22 12:51:51 -0500

    Update Form4.Designer.vb

--- signin/Form4.Designer.vb
+++ signin/Form4.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form4
+Partial Class Form3
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
