c3feff915152a0f574672a0ef868528cb7a7a9b4
jayuan101 <43624484+jayuan101@users.noreply.github.com>
2018-12-22 12:50:48 -0500

    Update Form1.vb

--- signin/Form1.vb
+++ signin/Form1.vb
@@ -1,4 +1,4 @@
-﻿Imports System.Data.OleDb
+Imports System.Data.OleDb
 
 Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click 'LOGIN BUTTON
@@ -7,15 +7,19 @@ Public Class Form1
             passtxt.Text = My.Settings.password Then
             MsgBox("Welcome, " & TextBox1.Text)
             Me.Hide()
-            Form3.Show()
+
         Else
             MsgBox("Wrong username or password!")
         End If
         'LOGIN BUTTON
 
         Timer1.Start() 'Timer on Form1.vb show
-            ProgressBar1.Show() 'Progress bar on Form1.vb show
-        Form5.Show()
+        ProgressBar1.Show() 'Progress bar on Form1.vb show
+
+
+        Form4.show()
+
+
 
     End Sub
 
@@ -26,7 +30,7 @@ Public Class Form1
     End Sub
 
     Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
-        Form4.Show()
+        Form3.Show()
     End Sub
 
 End Class
